3ffbd16f4d474c08f910b566fbf21ddba09a2cdf
NgDTrong <trongndph23292@fpt.edu.vn>
2025-07-17 17:57:42 +0700

    Initial commit

---
+++ app/src/main/java/app/movies/IntroActivity.java
@@ -0,0 +1,30 @@
+package app.movies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+private Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+        btnStart= findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/app/movies/LoginActivity.java
@@ -0,0 +1,44 @@
+package app.movies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText edtName,edtPass;
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+        edtName=findViewById(R.id.edtName);
+        edtPass=findViewById(R.id.edtPass);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtName.getText().toString().isEmpty()||edtPass.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                }else if (edtName.getText().toString().equals("trong")||edtPass.getText().toString().equals("trong")){
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }else {
+                    Toast.makeText(LoginActivity.this, "Không có tài khoản", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
