356e554085b20fa845f4c819676129ea22299340
NgDTrong <trongndph23292@fpt.edu.vn>
2025-07-19 17:46:00 +0700

    Thêm call api

--- app/src/main/java/app/movies/Activities/DetailActivity.java
+++ app/src/main/java/app/movies/Activities/DetailActivity.java
@@ -1,63 +1,88 @@
 package app.movies.Activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 
+import app.movies.Model.FilmItem;
 import app.movies.R;
 
 public class DetailActivity extends AppCompatActivity {
     private RequestQueue mRequestQueue;
     private StringRequest stringRequest;
     private ProgressBar mProgressBar;
-    private TextView tvTitle,tvMovieRatting,tvMovieTime,tvMovieSumary,tvMovieActor;
+    private TextView tvTitle, tvMovieRatting, tvMovieTime, tvMovieSumary, tvMovieActor;
     private int idFilm;
-    private ImageView imgTitle,imgBack,imgFav;
+    private ImageView imgTitle, imgBack, imgFav;
     private RecyclerView.Adapter adapterActorList, adapterCategory;
-    private RecyclerView recyclerActor,recyclerCategory;
+    private RecyclerView recyclerActor, recyclerCategory;
     private NestedScrollView scrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-        idFilm=getIntent().getIntExtra("id",0);
+        idFilm = getIntent().getIntExtra("id", 0);
         initView();
         sendRequest();
     }
 
     private void sendRequest() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        mProgressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+        stringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies/" + idFilm, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Gson gson = new Gson();
+                mProgressBar.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+                FilmItem item=gson.fromJson(s, FilmItem.class);
+                Glide.with(DetailActivity.this)
+                        .load(item.getPoster()).into(imgTitle);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        });
+        mRequestQueue.add(stringRequest);
     }
 
     private void initView() {
-        tvTitle=findViewById(R.id.tvMovieName);
-        mProgressBar=findViewById(R.id.progressBarDetail);
-        scrollView=findViewById(R.id.scrollView2);
-        imgTitle=findViewById(R.id.imgTitle);
-        tvMovieRatting=findViewById(R.id.tvRatting);
-        tvMovieTime=findViewById(R.id.tvTime);
-        tvMovieSumary=findViewById(R.id.tvMovieSumary);
-        tvMovieActor=findViewById(R.id.tvMovieActor);
-        imgBack=findViewById(R.id.btnBack);
-        imgFav=findViewById(R.id.btnFav);
-        recyclerActor=findViewById(R.id.rvImg);
-        recyclerActor.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-        recyclerCategory=findViewById(R.id.rvGenre);
-        recyclerCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-        imgBack.setOnClickListener(v-> finish());
+        tvTitle = findViewById(R.id.tvMovieName);
+        mProgressBar = findViewById(R.id.progressBarDetail);
+        scrollView = findViewById(R.id.scrollView2);
+        imgTitle = findViewById(R.id.imgTitle);
+        tvMovieRatting = findViewById(R.id.tvRatting);
+        tvMovieTime = findViewById(R.id.tvTime);
+        tvMovieSumary = findViewById(R.id.tvMovieSumary);
+        tvMovieActor = findViewById(R.id.tvMovieActor);
+        imgBack = findViewById(R.id.btnBack);
+        imgFav = findViewById(R.id.btnFav);
+        recyclerActor = findViewById(R.id.rvImg);
+        recyclerActor.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerCategory = findViewById(R.id.rvGenre);
+        recyclerCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        imgBack.setOnClickListener(v -> finish());
     }
 }
